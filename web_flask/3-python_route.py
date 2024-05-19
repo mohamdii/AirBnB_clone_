745fe9b05f4fd3867e7078934ce54c3c6f856a22
mohamdii <m.hamdy97@gmail.com>
2024-05-19 13:14:53 +0300

    styling fixted

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -19,6 +19,7 @@ def c_text(text):
     formatted_text = text.replace("_", " ")
     return "C " + formatted_text
 
+
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text='is cool'):
